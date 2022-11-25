152ccedc809f1f82afd074dda9c4ca1874beb2c3
alnickelzon <al.villanueva1234@gmail.com>
2022-11-25 14:42:24 +0800

    swiperjs

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,30 @@
+//swiper
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "swiper/css/scrollbar";
+import { yellow } from "./assets";
+
 const App = () => {
-  return <div></div>;
+  return (
+    <div className="root border-2">
+      <Swiper
+        className="swiper"
+        spaceBetween={50}
+        slidesPerView={1}
+        onSlideChange={() => console.log("slide change")}
+        onSwiper={(swiper) => console.log(swiper)}
+      >
+        <SwiperSlide>
+          <img src={yellow} alt="girl1" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={yellow} alt="girl1" />
+        </SwiperSlide>
+      </Swiper>
+    </div>
+  );
 };
 
 export default App;
