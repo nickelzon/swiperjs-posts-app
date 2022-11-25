4bd51d4965fd294aa0cc787eeb27a1ecb73e8304
alnickelzon <al.villanueva1234@gmail.com>
2022-11-25 20:28:32 +0800

    pagination

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,16 @@ import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
 import "swiper/css/scrollbar";
-import { yellow } from "./assets";
+import { yellow, gray } from "./assets";
+import { Pagination } from "swiper";
 
 const App = () => {
   return (
-    <div className="root border-2">
+    <div className="root border-2 h-[100vh] flex items-center border-red-300">
       <Swiper
-        className="swiper"
+        pagination={true}
+        modules={[Pagination]}
+        className="swiper mySwiper"
         spaceBetween={50}
         slidesPerView={1}
         onSlideChange={() => console.log("slide change")}
@@ -18,9 +21,14 @@ const App = () => {
       >
         <SwiperSlide>
           <img src={yellow} alt="girl1" />
+          <span className="text-[0.8rem]">Feeling pretty...</span>
         </SwiperSlide>
         <SwiperSlide>
-          <img src={yellow} alt="girl1" />
+          <img src={gray} alt="girl1" />
+          <span className="text-[0.8rem]">Feeling pretty too...</span>
+        </SwiperSlide>
+        <SwiperSlide>
+          Slide 3<span className="text-[0.8rem]">Use data to see photo...</span>
         </SwiperSlide>
       </Swiper>
     </div>
