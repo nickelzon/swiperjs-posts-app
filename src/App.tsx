62871daba4952671aa303665ecaf76dc2d74376c
alnickelzon <al.villanueva1234@gmail.com>
2022-11-27 13:37:17 +0800

    added heart reaction

--- src/App.tsx
+++ src/App.tsx
@@ -6,14 +6,19 @@ import "swiper/css/pagination";
 import "swiper/css/scrollbar";
 import { yellow, gray } from "./assets";
 import { Pagination } from "swiper";
+import { FaHeart } from "react-icons/fa";
+import { useState } from "react";
 
 const App = () => {
+  const [reaction, setReaction] = useState(false);
+
   return (
-    <div className="root border-2 h-[100vh] flex items-center border-red-300">
+    <div className="root h-[100vh] flex items-center">
       <Swiper
         pagination={true}
         modules={[Pagination]}
         className="swiper mySwiper"
+        loop={true}
         spaceBetween={50}
         slidesPerView={1}
         onSlideChange={() => console.log("slide change")}
@@ -21,11 +26,23 @@ const App = () => {
       >
         <SwiperSlide>
           <img src={yellow} alt="girl1" />
-          <span className="text-[0.8rem]">Feeling pretty...</span>
+          <div className={`flex flex-col items-center mb-10 `}>
+            <FaHeart
+              className={`mb-5 ${reaction ? "redheart" : ""}`}
+              onClick={() => setReaction(!reaction)}
+            />
+            <span className="text-[0.8rem]">Feeling pretty...</span>
+          </div>
         </SwiperSlide>
         <SwiperSlide>
           <img src={gray} alt="girl1" />
-          <span className="text-[0.8rem]">Feeling pretty too...</span>
+          <div className={`flex flex-col items-center mb-10 `}>
+            <FaHeart
+              className={`mb-5 ${reaction ? "redheart" : ""}`}
+              onClick={() => setReaction(!reaction)}
+            />
+            <span className="text-[0.8rem]">Feeling pretty too...</span>
+          </div>
         </SwiperSlide>
         <SwiperSlide>
           Slide 3<span className="text-[0.8rem]">Use data to see photo...</span>
